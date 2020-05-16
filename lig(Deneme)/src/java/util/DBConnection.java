e89ba562f6faf90743a8055334086c220e14da58
harunclsr <62938596+harunclsr@users.noreply.github.com>
2020-05-16 17:12:40 +0300

    Lig Modu

---
+++ lig(Deneme)/src/java/util/DBConnection.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+
+public class DBConnection {
+    public Connection connect(){
+        Connection c=null;
+        try{
+           
+            Class.forName("org.mariadb.jdbc.Driver").newInstance();
+           
+            c = DriverManager.getConnection("jdbc:mariadb://localhost:3306/turnuva?user=root&password=123");
+            
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return c;
+    }
+}
